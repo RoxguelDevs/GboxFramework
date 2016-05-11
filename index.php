ff522b8e0bee3db8cef5225a8ca5d7b8789f1602
Michel Varela <roxguel@gmail.com>
2016-05-11 14:57:46 -0300

    Debugger as default

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// defined('GBOX_DEBUG') or define('GBOX_DEBUG', true);
+defined('GBOX_DEBUG') or define('GBOX_DEBUG', true);
 
 require (__DIR__ . '/gbox/autoload.php');
 $config = require (__DIR__ . '/gbox/config/config.php');
