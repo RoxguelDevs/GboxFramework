22e086dbeccb9aeba7d21f51c33b43b8bb8bc1da
Michel Varela <roxguel@gmail.com>
2016-05-25 11:41:51 -0300

    New autoload

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ defined('GBOX_DEBUG') or define('GBOX_DEBUG', true);
 require (__DIR__ . '/gbox/autoload.php');
 $config = require (__DIR__ . '/gbox/config/config.php');
 
-$autoload = new Gbox\Autoload;
-$autoload->loadBase();
-$autoload->requireAll();
+$autoload = new Gbox\Autoload();
+// $autoload->loadBase();
+// $autoload->requireAll();
 
 (new Gbox($config))->run();
